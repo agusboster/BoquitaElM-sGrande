9269872a80ddf928527026d52cee75a100d307a1
agustinricardoluna <aluna@belatrixsf.com>
2019-10-11 12:24:08 -0300

    Alexis McAllister es de Boca y de Boca no se va

---
+++ src/main/java/com/BocaJuniors/turnosKinesiolog/a/TurnosKinesiologíaApplication.java
@@ -0,0 +1,13 @@
+package com.BocaJuniors.turnosKinesiolog.a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnosKinesiologíaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnosKinesiologíaApplication.class, args);
+	}
+
+}
